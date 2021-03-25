c65b90e6ea9c0469dd6f9f2e7e175987ed41883b
Tony Mogoa <mogoa.tonny@gmail.com>
2021-03-25 16:44:02 +0300

    Added method signatures.

---
+++ coa-assignment-2/src/numbersys/BaseConverter.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numbersys;
+
+/**
+ *
+ * @author tony
+ */
+public class BaseConverter {
+    
+    public static void main(String[] args){
+        
+    }
+    
+    public static String toBase2(int num){
+       return null; 
+    }
+    
+    public static String toBase2(float num){
+       return null; 
+    }
+    
+    public static String toBase16(int num){
+       return null; 
+    }
+    
+    public static void drawTable(){
+        
+    }
+    
+    public static void printInstructions(){
+        
+    }
+    
+    public static double[] genRandom(){
+        return null;
+    }
+}
