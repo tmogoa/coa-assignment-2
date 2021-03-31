576d5c5d755ed3fbb6839baf705fa48adfc20b9d
Tony Mogoa <mogoa.tonny@gmail.com>
2021-03-31 14:38:49 +0300

    Final commit

---
+++ coa-assignment-2/src/numbersys/Alignment.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numbersys;
+
+/**
+ *
+ * @author tony
+ */
+public enum Alignment {
+    RIGHT,
+    LEFT
+}
